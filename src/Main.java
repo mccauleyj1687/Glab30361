016c0a174116b68aed6dd2c84c1a800a36ceb36d
James Mcauley <mccauleyj1687@gmail.com>
2024-11-25 15:26:15 -0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        // create an array
+        int[] age = {12, 4, 5};
+
+        // loop through the array
+        // using for loop
+        System.out.println("Using for Loop:");
+        for(int i = 0; i < age.length; i++) {
+            System.out.println(age[i]);
+        }
+    }
+}
